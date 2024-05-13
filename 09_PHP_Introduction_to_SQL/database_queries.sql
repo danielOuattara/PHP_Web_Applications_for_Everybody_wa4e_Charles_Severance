ccb7d7e87e4ad42376b16a4f68eae1dbe521dff1
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-13 21:54:50 +0200

    09 PHP intro to SQL: create `database_queries.sql`

---
+++ 09_PHP_Introduction_to_SQL/database_queries.sql
@@ -0,0 +1,68 @@
+-- 2 cases where to run the following commands: 
+-- 1) pur MySQL , using console or any available GUI
+-- 2) MySQL(Maria) with XAMPP, through phpMyAdmin
+
+-- connect as root; enter the password when prompted
+mysql -u root -p 
+
+-- create a user
+CREATE USER IF NOT EXISTS 'daniel'@'localhost' IDENTIFIED BY '**Azerty77**';
+
+-- grant in advance all privileges on databases that user 'daniel' could created
+GRANT ALL PRIVILEGES ON music.* TO 'daniel'@'localhost';
+GRANT ALL PRIVILEGES ON people.* TO 'daniel'@'localhost';
+
+
+-- disconnect
+quit
+
+-- connect as user 'daniel';  enter the password when prompted
+mysql -u daniel -p
+
+-- created the databases:
+CREATE DATABASE music people DEFAULT CHARACTER SET utf8;
+
+
+-- connect to `people`database
+USE people;
+
+-- create talbe Users:
+CREATE TABLE Users (
+  name VARCHAR(128), 
+  email VARCHAR(128)
+);
+
+
+-- confirm Users table created
+DESCRIBE Users;
+
+-- add users: 
+INSERT INTO Users (name, email) VALUES ('Chuck', 'csev@umich.edu') ;
+INSERT INTO Users (name, email) VALUES ('Sally', 'sally@umich.edu') ;
+INSERT INTO Users (name, email) VALUES ('Somesh', 'somesh@umich.edu') ;
+INSERT INTO Users (name, email) VALUES ('Caitlin', 'cait@umich.edu') ;
+INSERT INTO Users (name, email) VALUES ('Ted', 'ted@umich.edu') ;
+
+
+-- delete one user:
+DELETE FROM Users WHERE email='ted@umich.edu';
+
+-- update one user:
+UPDATE Users SET name='Charles' WHERE email='csev@umich.edu';
+
+-- simple queries: retrieve records
+SELECT * FROM Users;
+
+SELECT * FROM Users WHERE email='csev@umich.edu';
+
+SELECT * FROM Users ORDER BY email;
+
+SELECT * FROM Users WHERE name LIKE '%e%';
+
+SELECT * FROM Users ORDER BY email DESC LIMIT 2;
+
+SELECT * FROM Users ORDER BY email LIMIT 1,2;
+
+SELECT COUNT(*) FROM Users;
+
+SELECT COUNT(*) FROM Users WHERE email='csev@umich.edu';
